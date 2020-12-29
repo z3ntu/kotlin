253854d961207f2b9cda5a074fffda8a55a13332
Iaroslav Postovalov <postovalovya@gmail.com>
2020-12-30 01:18:32 +0700

    Add before/after test of PackagePrivateFileClassesWithAllPrivateMembers

---
+++ compiler/testData/codegen/bytecodeListing/fileClassWithPrivateDeclarationsOnly_after.kt
@@ -0,0 +1,9 @@
+// WITH_RUNTIME
+// IGNORE_BACKEND: JVM
+// !LANGUAGE: +PackagePrivateFileClassesWithAllPrivateMembers
+
+private fun f() {
+}
+
+private val a = Unit
+private val b by lazy { Unit }
